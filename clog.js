3efb3c00b511692de853470e552268c10a91bbd4
Gusla Shidiq <46296234+guslashidiq@users.noreply.github.com>
2019-01-03 14:08:30 +0700

    Add files via upload

---
+++ clog.js
@@ -0,0 +1,26 @@
+console.log("====================================");
+console.log("	Welcome To Gorontalo Website	");
+console.log("===================================");
+console.log("		UAS Pemrograman Web 	 	");
+console.log("===================================");
+console.log(10+3);
+console.log(10-3);
+console.log(10*3);
+function perkalian (x,y)
+{
+  var hasil = x * y;
+  return hasil;
+}
+console.log(perkalian(3,4));
+function myFunction()
+{
+  myOtherFunction();
+}
+function myOtherFunction()
+{
+  console.trace();
+}
+
+console.time("Running Time");
+console.timeEnd("Running Time");
+ 
---
+++ popup.js
@@ -0,0 +1,9 @@
+var konfirmasi = confirm("Apakah Anda ingin membuka website resmi Provinsi Gorontalo?")
+if(konfirmasi==true)
+{
+  document.location.href="##";
+}
+else
+{
+  document.location.href="";
+}
